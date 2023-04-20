945f251e30a2a91d19e9eec2f888c8380806b406
ChenElla <cyqfypex@gmail.com>
2023-04-20 19:35:50 -0400

    Update colors.scss and generalTheme.js with colors

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import React from 'react'
+export default function Header() {
+  return (
+    <div>
+        <Button 
+            // colorScheme = 'green'
+            bg = 'green.200'
+            width = "130px"
+        >BUTTON</Button>
+    </div>
+  )
+}
