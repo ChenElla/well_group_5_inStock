477521cdfde1e60494fc590ca0ed656de2b22931
ChenElla <cyqfypex@gmail.com>
2023-04-20 20:06:32 -0400

    Reformat the theme to use for Chakra

---
+++ src/styling/theme/index.js
@@ -0,0 +1,21 @@
+import { extendTheme } from "@chakra-ui/react";
+
+//Global style overrides
+import styles from "./styles";
+
+//Foundational style overrides
+
+
+
+//Component style overrides
+import Button from "./components/button"
+
+const overrides = {
+    styles,
+    components:{
+        Button,
+        //Other components go here
+    },
+}
+
+export default extendTheme(overrides)
