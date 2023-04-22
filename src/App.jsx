90dc88eada2b033ed1e865fb987603b8598958dc
laithajjan <laithajjan@gmail.com>
2023-04-21 18:38:18 -0600

    Added footer component, no styling yet

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import Footer from './components/footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +28,7 @@ function App() {
             Edit <code>src/App.jsx</code> and save to test HMR
           </p>
         </div>
+        <Footer />
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+
+    return (
+       <section className="footer">
+        <p className="p3">© InStock Inc. All Rights Reserved.</p>
+       </section>
+
+    );
+}
+
+export default Footer;
