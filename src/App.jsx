e738a47c150a49f9a5fdf5dc97e5ce9446fb8092
Will <wgale4@gmail.com>
2023-04-23 13:54:11 -0400

    created GLobal.scss file and removed padding/margin from body

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./Global.scss";
 import Header from "./components/Header/Header";
 
 function App() {
