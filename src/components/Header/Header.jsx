73c69bc4474f57de41f28aa738a322bf1564892d
Will <wgale4@gmail.com>
2023-04-23 01:31:07 -0400

    aded media queries and padding to make page consistent with mobile and tablet in terms of playout. Added mixins to top of page which need relocating

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,33 +7,34 @@ import "./Header.scss";
 function Header() {
   return (
     <div className="header">
-      {/* can I wrap two seperate items in a link tag? Especially given only 1
-      will show at any time? */}
-      <div className="header__logo-contain">
-        <Link to="/">
-          <img
-            className="header__logo header__logo--med"
-            src={instockLogoSmall}
-            alt="Instock Logo"
-          />
-          <img
-            className="header__logo header__logo--lrg"
-            src={instockLogoMedium}
-            alt="Instock Logo"
-          />
-        </Link>
-      </div>
-      <div className="header__btn-contain">
-        <Link to="/warehouse">
-          <button className="header__btn header__btn--warehouse">
-            Warehouse
-          </button>
-        </Link>
-        <Link to="/inventory">
-          <button className="header__btn header__btn--inventory">
-            Inventory
-          </button>
-        </Link>
+      <div className="header__outer">
+        <div className="header__logo-contain">
+          {/* can I wrap two seperate items in a link tag? Especially given only 1 will show at any time? Seems I can with initial testing */}
+          <Link to="/">
+            <img
+              className="header__logo header__logo--sml"
+              src={instockLogoSmall}
+              alt="Instock Logo"
+            />
+            <img
+              className="header__logo header__logo--med"
+              src={instockLogoMedium}
+              alt="Instock Logo"
+            />
+          </Link>
+        </div>
+        <div className="header__btn-contain">
+          <Link to="/warehouse">
+            <button className="header__btn header__btn--warehouse">
+              Warehouse
+            </button>
+          </Link>
+          <Link to="/inventory">
+            <button className="header__btn header__btn--inventory">
+              Inventory
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
