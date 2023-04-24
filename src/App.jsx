294c8f4ac74e65cb5076c4b236bda53d7bf6bf2a
Will <wgale4@gmail.com>
2023-04-23 20:12:33 -0400

    created component PageHeader and imported in App.jsx for testing. Experiemmented with customizing components and passed sting into component from App.js with title of page. Want to know if this is the viable way to make component customizable. Saving and will branch off so have a return point

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./Global.scss";
 import Header from "./components/Header/Header";
+import PageHeader from "./components/PageHeader/PageHeader";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <PageHeader pageHeader={"Add New Inventory Item"} />
         {/* <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/video/:videoId" element={<HomePage />} />
---
+++ src/components/PageHeader/PageHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PageHeader({ pageHeader }) {
+  return <div>{pageHeader}</div>;
+}
+
+export default PageHeader;
