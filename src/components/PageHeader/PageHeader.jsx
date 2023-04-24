70dc6bd67942fa482919e76f3828cd9d74336199
Will <wgale4@gmail.com>
2023-04-23 21:03:14 -0400

    styled Page header component. Padding issue for top/bottom persists

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import backArrow from "../../assets/Icons/arrow_back-24px.svg";
+import "./PageHeader.scss";
 
 function PageHeader({ pageHeader }) {
-  return <div>{pageHeader}</div>;
+  return (
+    <div className="pageHeader">
+      <div className="pageHeader__outer">
+        <div className="pageHeader__inner-container">
+          <img
+            className="pageHeader__return"
+            src={backArrow}
+            alt="Back Arrow"
+          />
+          <h1 className="pageHeader__title-text">{pageHeader}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PageHeader;
