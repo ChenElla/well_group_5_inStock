40317545ee1d06a6bc10eff230d7c6e48e5e1047
Logan Henrie <loghenrie@gmail.com>
2023-04-26 01:53:21 -0400

    fix case of import paths

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import loading_icon from "../../assets/icons/Loading_icon.gif";
+import loading_icon from "../../assets/Icons/Loading_icon.gif";
 import "./Loading.scss";
 
 export default function Loading() {
-  return (
-    <div className="loadingContainer">
-      <img
-        className="loadingContainer__icon"
-        src={loading_icon}
-        alt="loading-icon"
-      />
-      <div className="loadingContainer__loading">Loading ... </div>
-    </div>
-  );
+	return (
+		<div className="loadingContainer">
+			<img
+				className="loadingContainer__icon"
+				src={loading_icon}
+				alt="loading-icon"
+			/>
+			<div className="loadingContainer__loading">Loading ... </div>
+		</div>
+	);
 }
