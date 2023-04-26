b6c1e05a566892460f919e6b6b527b414134fe7d
Logan Henrie <loghenrie@gmail.com>
2023-04-26 02:23:03 -0400

    change from extra prop to using native children prop

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -5,7 +5,7 @@ import "./PageHeader.scss";
 
 //may want to rename this component to something more specific as may nbeed to make another 2 for the different styles. THOUGH those are more elaborate so they could be modified names i.e. PageHeaderSearch or PageHeaderEdit (which are 2 other styles)
 
-function PageHeader({ pageHeader, extra }) {
+function PageHeader({ pageHeader, children }) {
 	// function to allow user to go back to last page they were on - untested as routes not set up yet, though works if change url via warehouse/inventory buttons
 	const navigate = useNavigate();
 	const goBack = () => {
@@ -23,7 +23,7 @@ function PageHeader({ pageHeader, extra }) {
 						onClick={goBack}
 					/>
 					<h1 className="pageHeader__title-text">{pageHeader}</h1>
-					{extra}
+					{children}
 				</div>
 			</div>
 		</div>
