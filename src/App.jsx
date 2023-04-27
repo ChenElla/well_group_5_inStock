75927e0a0e5d76f0add16f88089e9b96a286afe1
Will <wgale4@gmail.com>
2023-04-26 21:28:26 -0400

    fixed conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import NewInvBody from "./components/NewInvBody/InvItemBody";
 import InventoryDetails from "./components/InventoryDetails/inventorydetails";
 
 function App() {
+  const Placeholder = ({ text }) => (
+    <div style={{ padding: "2rem", textAlign: "center" }}>{text}</div>
+  );
+
   return (
     <div className="root">
       <BrowserRouter>
@@ -14,13 +18,31 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Placeholder text="Warehouses" />} />
-            <Route path="/inventories" element={<Placeholder text="Inventories" />} />
-            <Route path="/warehouses/:id" element={<Placeholder text="Warehouse Details" />} />
+            <Route
+              path="/inventories"
+              element={<Placeholder text="Inventories" />}
+            />
+            <Route
+              path="/warehouses/:warehouseId"
+              element={<WarehouseDetailsPage />}
+            />
             <Route path="/inventories/:id" element={<InventoryDetails />} />
-            <Route path="/warehouses/:id/edit" element={<Placeholder text="Edit Warehouse" />} />
-            <Route path="/warehouses/add" element={<Placeholder text="Add Warehouse" />} />
-            <Route path="/inventories/:id/edit" element={<Placeholder text="Edit Inventory" />} />
-            <Route path="/inventories/add" element={<Placeholder text="Add Inventory" />} />
+            <Route
+              path="/warehouses/:id/edit"
+              element={<Placeholder text="Edit Warehouse" />}
+            />
+            <Route
+              path="/warehouses/add"
+              element={<Placeholder text="Add Warehouse" />}
+            />
+            <Route
+              path="/inventories/:id/edit"
+              element={<Placeholder text="Edit Inventory" />}
+            />
+            <Route
+              path="/inventories/add"
+              element={<Placeholder text="Add Inventory" />}
+            />
           </Routes>
           <Footer />
         </div>
