ae2f64e63b6dadc4e700ec76dd2b24116beae0e6
ChenElla <cyqfypex@gmail.com>
2023-04-26 23:11:49 -0400

    Removed footer

--- src/components/Footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Footer.scss';
-export default function Footer() {
-    <>
-      <footer className="footer">
-        <p className="footer__text">© InStock Inc. All Rights Reserved.</p>
-      </footer>
-    </>
-}
