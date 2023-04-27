d12269074dd89a295573857d1f3bf45bf87da537
Logan Henrie <loghenrie@gmail.com>
2023-04-27 15:43:05 -0400

    make back button a prop and set cursor:pointer on back button

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -5,7 +5,7 @@ import "./PageHeader.scss";
 
 //may want to rename this component to something more specific as may nbeed to make another 2 for the different styles. THOUGH those are more elaborate so they could be modified names i.e. PageHeaderSearch or PageHeaderEdit (which are 2 other styles)
 
-function PageHeader({ pageHeader, children }) {
+function PageHeader({ pageHeader, children, backButton = true }) {
 	// function to allow user to go back to last page they were on - untested as routes not set up yet, though works if change url via warehouse/inventory buttons
 	const navigate = useNavigate();
 	const goBack = () => {
@@ -16,12 +16,14 @@ function PageHeader({ pageHeader, children }) {
 		<div className="pageHeader">
 			<div className="pageHeader__outer">
 				<div className="pageHeader__inner-container">
-					<img
-						className="pageHeader__return"
-						src={backArrow}
-						alt="Back Arrow"
-						onClick={goBack}
-					/>
+					{backButton && (
+						<img
+							className="pageHeader__return"
+							src={backArrow}
+							alt="Back Arrow"
+							onClick={goBack}
+						/>
+					)}
 					<h1 className="pageHeader__title-text">{pageHeader}</h1>
 					{children}
 				</div>
