c64c92eb70d70ecf07f5ee888245ad2f6c250f26
Will <wgale4@gmail.com>
2023-04-27 21:59:22 -0400

    attempting to make stock dynamic 1. not working

--- src/components/ItemAvailability/ItemAvailability.jsx
+++ src/components/ItemAvailability/ItemAvailability.jsx
@@ -15,16 +15,7 @@ function ItemAvailability({
   setWarehouse,
 }) {
   const [warehouseList, setWarehouseList] = useState("");
-  //   HAve to do get request to get info for warehouses
-  //   SELECT distinct category
-  // FROM `instock`.`inventories`
-
-  // TO DO
-  // need to get all warehouses. Filter for id and city. set those as state. need to conflate id and city name.
-
-  //localhost:5050/warehouses/
-
-  //SHOULD I BE MAKING A COMPONENT FOR THE DROP DOWN BOX AND DO ALL THIS IN THAT?
+  const [displayQuantity, setDisplayQuantity] = useState("");
 
   useEffect(() => {
     axios
@@ -40,19 +31,19 @@ function ItemAvailability({
           warehouse_name,
         }));
 
-        // const uniqueWarehouseNames = [
-        //   ...new Set(cityAndID.map((cityAndID) => cityAndID.warehouse_name)),
-        // ];
-
-        // console.log("here is uniqueWarehouseNames", uniqueWarehouseNames);
-
-        setWarehouseList(cityAndID); //I like having key value since selecting the city will require the appropriate id to be entered in the post request
+        setWarehouseList(cityAndID);
       })
       .catch((err) => {
         console.log(err);
         // navigate("/");
       });
   }, []);
+
+  const radioHandler = (e) => {
+    setStatus("Out of Stock");
+    setQuantity("0");
+    setShowQuantity(false);
+  };
 
   console.log("here is warehouse list", warehouseList);
   if (warehouseList)
@@ -80,24 +71,31 @@ function ItemAvailability({
                     type="radio"
                     className="available__radio-btn"
                     value={status}
-                    onChange={(e) => setStatus("In Stock")}
+                    onChange={(e) => radioHandler}
+                    // onChange={(e) => {
+                    //   setStatus("Out of Stock");
+                    //   setQuantity("0");
+                    // }}
                     name="stock"
                   ></input>
                   <label className="available__radio-text">Out of stock</label>
                 </div>
               </div>
             </div>
-            <div className="available__quantity-package">
-              <p className="available__label">Quantity</p>
-              <input
-                className="available__input item available__input--quantity"
-                type="number"
-                placeholder="0"
-                value={quantity}
-                onChange={(e) => setQuantity(e.target.value)}
-                required
-              />
-            </div>
+            {/* <input type="text" onChange={radioHandler} /> */}
+            {displayQuantity && (
+              <div className="available__quantity-package">
+                <p className="available__label">Quantity</p>
+                <input
+                  className="available__input item available__input--quantity"
+                  type="number"
+                  placeholder="0"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                  required
+                />
+              </div>
+            )}
             <div className="available__warehouse-package">
               <p className="available__label">Warehouse</p>
               <div className="available__arrow-container">
